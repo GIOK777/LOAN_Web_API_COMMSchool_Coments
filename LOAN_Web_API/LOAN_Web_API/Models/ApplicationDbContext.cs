36e3e3f3e1f735afb25e4836f137011b087d0f89
Giorgi Kitiashvili <gioqitiashvili@yahoo.com>
2025-11-17 22:52:44 +0400

    upload project

---
+++ LOAN_Web_API/LOAN_Web_API/Models/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LOAN_Web_API.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+    }
+    
+}
